bf9f558cac99f2cec9a2dde49e7b88a625711954
Dzikril Hakim <dzikril.hakim@iconpln.co.id>
2024-11-08 15:07:25 +0700

    ga ignore

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,53 +1,54 @@
 <div class="sticky-top">
-        <header class="navbar navbar-expand-md sticky-top d-print-none" style="background-color" >
-          <div class="container-xl" style="align-items: flex-end;">
-              <a href=".">
-                Login Admin
-              </a>
-          </div>
-        </header>
-        <header class="navbar-expand-md">
-          <div class="collapse navbar-collapse" id="navbar-menu">
+    <header class="navbar navbar-expand-md sticky-top d-print-none">
+        <div class="container-xl" style="align-items: flex-end;">
+            <a href="#" style="font-size: 12px; color: #103152;">
+            Login Admin
+            </a>
+        </div>
+        
+    </header>
+    <header class="navbar-expand-md">
+        <div class="collapse navbar-collapse" id="navbar-menu">
             <div class="navbar">
                 <div class="container-xl">
                     <div class="col-6">
                         <div class="row">
-                        <div class="nav-item dropdown">
-              <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu" aria-expanded="false">
-                <span class="avatar avatar-sm" style="background-image: url({{asset('tabler/static/avatars/logo.png')}})"></span>
-                <div class="d-none d-xl-block ps-2">
-                  <div>Coorporate Portal</div>
-                </div>
-              </a>
-            </div>
+                            <div class="nav-item dropdown">
+                                <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu" aria-expanded="false">
+                                    <span class="avatar avatar-sm" style="background-image: url({{asset('tabler/static/avatars/logo.png')}}); box-shadow: none; background-color: #1A8499;"></span>
+                                    <div class="d-none d-xl-block ps-2">
+                                    <div style="font-size: 16px; color: #FFFFFF; font-weight: bold;">Coorporate Portal</div>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div class="col-6">
                         <ul class="navbar-nav" style="justify-content: flex-end;">
                             <li class="nav-item">
-                                <a class="nav-link" href="./" >
-                                <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
-                                </span>
-                                <span class="nav-link-title">
-                                    Aplikasi
-                                </span>
+                                <a class="nav-link" href="#" >
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                                    </span>
+                                    <span class="nav-link-title" style="font-size: 16px; color: #FFFFFF;">
+                                        Aplikasi
+                                    </span>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="./" >
-                                <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
-                                </span>
-                                <span class="nav-link-title">
-                                    Link Meeting
-                                </span>
+                                <a class="nav-link" href="#" >
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                                    </span>
+                                    <span class="nav-link-title" style="font-size: 16px; color: #FFFFFF;">
+                                        Link Meeting
+                                    </span>
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </div>              
             </div>
-          </div>
-        </header>
-      </div>
+        </div>
+    </header>
+</div>
